799fb69c1697f89ee1d12ad688e7f67b3f3787f2
Dinoco <estherdavila2004@gmail.com>
2026-05-21 10:35:04 -0600

    Añadir definición de clase de juego con integración GLFW

--- src/Game.h
+++ src/Game.h
@@ -1,1 +1,19 @@
-. 
+#ifndef GAME_H
+#define GAME_H
+
+#include <vendor/glad/glad.h> // <- ¡IMPORTANTE! GLAD siempre va primero
+#include <GLFW/glfw3.h>
+
+class Game {
+private:
+    GLFWwindow* window;
+
+public:
+    Game();
+    ~Game();
+    void init(GLFWwindow* currentWindow);
+    void update(float deltaTime);
+    void render();
+};
+
+#endif
